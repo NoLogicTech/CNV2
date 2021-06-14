dc21e58e00bc4adad73e43b59b2a50a35c3592da
Miguel Correia <geral@nologic.pt>
2021-06-14 21:31:56 +0100

    tt

--- obj/Debug/net5.0/RazorDeclaration/Pages/TCliente.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/TCliente.razor.g.cs
@@ -13,91 +13,91 @@ namespace CNV2.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Repositorios\CNV2\_Imports.razor"
+#line 1 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Repositorios\CNV2\_Imports.razor"
+#line 2 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Repositorios\CNV2\_Imports.razor"
+#line 3 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Repositorios\CNV2\_Imports.razor"
+#line 4 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Repositorios\CNV2\_Imports.razor"
+#line 5 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Repositorios\CNV2\_Imports.razor"
+#line 6 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Repositorios\CNV2\_Imports.razor"
+#line 7 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Repositorios\CNV2\_Imports.razor"
+#line 8 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Repositorios\CNV2\_Imports.razor"
+#line 9 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using CNV2;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Repositorios\CNV2\_Imports.razor"
+#line 10 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using CNV2.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\Repositorios\CNV2\_Imports.razor"
+#line 11 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Repositorios\CNV2\Pages\TCliente.razor"
+#line 2 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\Pages\TCliente.razor"
 using System.Text.RegularExpressions;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Repositorios\CNV2\Pages\TCliente.razor"
+#line 3 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\Pages\TCliente.razor"
 using System.ComponentModel.DataAnnotations;
 
 #line default
@@ -112,7 +112,7 @@ using System.ComponentModel.DataAnnotations;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 27 "D:\Repositorios\CNV2\Pages\TCliente.razor"
+#line 27 "C:\Users\NoLogicTech\Documents\Repos\ASerio\CNV2\Pages\TCliente.razor"
        
     bool success;
     string[] errors = { };
